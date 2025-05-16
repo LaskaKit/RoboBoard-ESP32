0c70d64e4e8a0fef837a914d8523158f194fbb5c
Patrik Novak <novakpatrik.99@protonmail.com>
2025-05-16 14:57:23 +0200

    Implement secure communication. Refactor.

--- SW/omni_wheels_example/fw_laska_omni_car/fw_laska_omni_car.ino
+++ SW/omni_wheels_example/fw_laska_omni_car/fw_laska_omni_car.ino
@@ -1,32 +1,40 @@
-/* 
+/*
   Example code for LaskaKit RoboBoard ESP32. This example contains
   the FW for the 'car' with omni wheels that is controlled from gamepad
   using esp-now protocol.
-  
-  Board:  LaskaKit RoboBoard ESP32      https://www.laskakit.cz/laskakit-roboboard-esp32/?variantId=13424
-  Motors: TT motor - plastic gears      https://www.laskakit.cz/tt-motor-s-prevodovkou-plastove-prevody/
-  Wheels: Mecanum Omni Wheels           https://www.laskakit.cz/sada-4ks-mecanum-omni-kol-60mm/
 
-  Email: podpora@laskakit.cz
-  Web: laskakit.cz
+  Arduino IDE setting
+    Board: ESP32 Dev Module
+
+  Board:   LaskaKit RoboBoard ESP32
+           https://www.laskakit.cz/laskakit-roboboard-esp32/?variantId=13424
+  Motors:  TT motor - steel gears
+           https://www.laskakit.cz/tt-motor-s-prevodovkou-plastove-prevody/ 
+  Wheels:  Mecanum Omni Wheels
+           https://www.laskakit.cz/sada-4ks-mecanum-omni-kol-60mm/
+  Display: LaskaKit OLED display 128x64 1.3"
+           https://www.laskakit.cz/laskakit-oled-displej-128x64-1-3--i2c/
+  LEDs:    8x intelligent RGB LED NeoPixel strip
+           https://www.laskakit.cz/8x-inteligentni-rgb-led-neopixel-pasek--ws2812b--5050--5v/
+
+  Email:   podpora@laskakit.cz
+  Web:     laskakit.cz
 */
 
-#include <Adafruit_MotorShield.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_MotorShield.h>
 #include <Adafruit_SH110X.h>
 #include <ESP32AnalogRead.h>
-#include <esp_now.h>
 #include <WiFi.h>
+#include <esp_now.h>
 
-#include "laskagamepad.hpp"
 #include "laskacar.hpp"
 #include "laskacar_beacon.hpp"
+#include "laskagamepad.hpp"
 
-#define CHANNEL 1
-#define DIVIDER_RATIO 2.599   //new
-// 8.4 100
-// 6.0   0
-
+#define DIVIDER_RATIO 2.599
+// 8.4V 100%
+// 6.0V   0%
 
 #define ADCpin 34
 ESP32AnalogRead adc;
@@ -43,11 +51,12 @@ Adafruit_DCMotor *BackLeftMotor = AFMS.getMotor(4);
 Adafruit_DCMotor *BackRightMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *FrontRightMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *FrontLeftMotor = AFMS.getMotor(2);
-LaskaOmniCar laska_omni_car(BackLeftMotor, BackRightMotor, FrontLeftMotor, FrontRightMotor);
+LaskaOmniCar laska_omni_car(BackLeftMotor, BackRightMotor, FrontLeftMotor,
+                            FrontRightMotor);
 
 struct Message {
   LaskaGamepad gamepad_inputs;
-  uint8_t battery_v;  // voltage * 10
+  uint8_t battery_v; // voltage * 10
   uint8_t off;
 };
 Message m;
@@ -60,35 +69,31 @@ LeftRightBeaconPattern lrbp(&beacon);
 
 Adafruit_SH1106G display(128, 64, &Wire);
 
-
 int display_refresh = 0;
 unsigned long gamepad_watchdog_timer = 0;
 
-
+// here specify the mac address of your gamepad
+// the mac address can be optained from running
+// the get_mac_address sketch
 uint8_t gamepads[2][6] = {
-  {0x80, 0x65, 0x99, 0x96, 0x6B, 0x3C},  // black
-  {0x24, 0x58, 0x7C, 0x01, 0xF9, 0xA8} // pink
+    {0x80, 0x65, 0x99, 0x96, 0x6B, 0x3C}, // black
+    {0x24, 0x58, 0x7C, 0x01, 0xF9, 0xA8}  // pink
 };
 
+uint8_t active_gamepad = 0;
+
+// type your super secret keys here (16 bytes each)
+static const char *PMK_KEY_STR = "laskakit_pmk_key";
+static const char *LMK_KEY_STR = "laskakit_lmk_key";
 esp_now_peer_info_t peerInfo;
-
-
-bool mac_compare(const uint8_t mac1[6], const uint8_t mac2[6]) {
-  for (int i = 0; i < 1; i++) {
-    if (mac1[i] != mac2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 
 // esp-now callback
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   // char macStr[18];
   // snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-  //          mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  memcpy(&m, data, sizeof(m));
+  //          mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4],
+  //          mac_addr[5]);
+  // memcpy(&m, data, sizeof(m));
 
   if (data_len != 12) {
     return;
@@ -99,7 +104,6 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
                       m.gamepad_inputs.left_stick_x >> 7);
   gamepad_watchdog_timer = millis();
 }
-
 
 uint8_t get_battery_status() {
   float empty = 6.0, full = 8.4;
@@ -115,12 +119,10 @@ void display_battery_status() {
   display.print("%");
 }
 
-
-void poweroff(void){
-  tone(PIN_BUZZ,500,500);
+void poweroff(void) {
+  tone(PIN_BUZZ, 500, 500);
   digitalWrite(AUTO_OFF, HIGH);
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -132,19 +134,10 @@ void setup() {
   // display
   display.begin(0x3C, true); // Address 0x3C default
   display.setTextColor(SH110X_WHITE);
-  // display.setTextSize(1);
-  // display.setCursor(0, 0);
-  // display.print("Battery voltage:");
   display.setTextSize(5);
 
   // battery voltage measurement
   adc.attach(ADCpin);
-  // battery_car_v = adc.readVoltage() * DIVIDER_RATIO;
-  // display.clearDisplay();
-  // display.setCursor(0, 20);
-  // display.print(battery_car_v);
-  // Serial.println(battery_car_v);
-
   display_battery_status();
 
   WiFi.mode(WIFI_STA);
@@ -154,6 +147,7 @@ void setup() {
   } else {
     Serial.println("esp now OK");
   }
+  esp_now_set_pmk((uint8_t *)PMK_KEY_STR);
 
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info.
@@ -162,30 +156,26 @@ void setup() {
   // start adafruit motor shield
   AFMS.begin();
 
-
-  // init the beacon pattern
-  // lrbp.init();
+  // setup peer info
+  memcpy(peerInfo.peer_addr, gamepads[active_gamepad], 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = true;
+  memcpy(peerInfo.lmk, LMK_KEY_STR, 16);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+  }
 
   // signal that setup is complete
-  tone(PIN_BUZZ,1000,200);
-
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  memcpy(peerInfo.peer_addr, gamepads[0], 6);
-  esp_now_add_peer(&peerInfo);
-  // memcpy(peerInfo.peer_addr, gamepads[1], 6);
-  // esp_now_add_peer(&peerInfo);
+  tone(PIN_BUZZ, 1000, 200);
 }
-
 
 void loop() {
   // gamepad connection watchdog
   unsigned long current = millis();
   if (current - gamepad_watchdog_timer >= 200) {
-    laska_omni_car.move(0, 0, 0);  // stop the car
-    m.gamepad_inputs.buttons = 0;  // unpress all buttons
+    laska_omni_car.move(0, 0, 0); // stop the car
+    m.gamepad_inputs.buttons = 0; // unpress all buttons
   }
-
 
   display_refresh++;
   // refresh once every 10s
@@ -211,7 +201,8 @@ void loop() {
   }
 
   // power off the car when select is pressed or battery is <= 30%
-  if (m.gamepad_inputs.get_button(LaskaGamepadButton::START) { // ||  get_battery_status() <= 30) {
+  if (m.gamepad_inputs.get_button(
+          LaskaGamepadButton::START)) { // ||  get_battery_status() <= 30) {
     poweroff();
   }
 
@@ -228,5 +219,3 @@ void loop() {
   beacon.leds.show();
   delay(50);
 }
-
-
--- SW/omni_wheels_example/fw_laska_omni_gamepad/fw_laska_omni_gamepad.ino
+++ SW/omni_wheels_example/fw_laska_omni_gamepad/fw_laska_omni_gamepad.ino
@@ -1,13 +1,23 @@
+/*
+  Example code for the LaskaKit gamepad based on ESP32C3
+  which controls the `car` with omni wheels based od LaskaKit RoboBoard ESP32.
+
+  Arduino IDE setting
+    Board: ESP32C3 Dev Module
+
+  Email: podpora@laskakit.cz
+  Web:   laskakit.cz
+*/
+
+#include "ADS1X15.h" //Rob Tillaart ADS1X15 Library
+#include "PCF8575.h" //Rob Tillaart PCF8575 Library
 #include <Adafruit_NeoPixel.h>
-#include <ESP32AnalogRead.h> 
-#include "ADS1X15.h"  //Rob Tillaart ADS1X15 Library
-#include "PCF8575.h"  //Rob Tillaart PCF8575 Library
-#include <Wire.h>
+#include <ESP32AnalogRead.h>
 #include <WiFi.h>
+#include <Wire.h>
 #include <esp_now.h>
 
 #include "laskagamepad.hpp"
-
 
 // adc sticks, L2, R2
 ADS1115 ADS(0x49);
@@ -21,41 +31,37 @@ PCF8575 PCF(0x20);
 #define NUM_LED 3
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LED, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-
 // battery
 ESP32AnalogRead adc;
 #define ADCpin 0
 #define DividerRatio 1.7693877551
 
-
 // hw button?
 #define OFF_PIN 5
 
+// here specify the mac address of your robo board (car)
+// the mac address can be optained from running
+// the get_mac_address sketch
 uint8_t cars[3][6] = {
-  // 08:D1:F9:C4:3B:A4
-  {0xA8, 0x42, 0xE3, 0x80, 0xE2, 0xC4},  // blue car
-  {0x08, 0xD1, 0xF9, 0xC4, 0x3B, 0xA4},  // red car
-  {0x08, 0xD1, 0xF9, 0xC4, 0x60, 0xE4}   // my esp for testing
-  // {0xA8, 0x42, 0xE3, 0x80, 0xE2, 0xC4}  // blue car A8:42:E3:80:E2:C4
+    {0xA8, 0x42, 0xE3, 0x80, 0xE2, 0xC4}, // backup robo board
+    {0x08, 0xD1, 0xF9, 0xC4, 0x3B, 0xA4}, // red car
+    {0x08, 0xD1, 0xF9, 0xC4, 0x60, 0xE4}  // blue car
 };
 
-uint8_t active_car = 0;
+uint8_t active_car = 2;
 
-// napad: prepinat slave adresy `select` tlacitkem
-// uint8_t slaveAddress[] = {0x08, 0xD1, 0xF9, 0xC4, 0x3B, 0xA4};  //cervene auto 08:D1:F9:C4:3B:A4
-// uint8_t slaveAddress[] = {0xA8, 0x42, 0xE3, 0x80, 0xE2, 0xC4};  //modre auto  A8:42:E3:80:E2:C5 A8:42:E3:80:E2:C4
-// uint8_t slaveAddress[] = {0x08, 0xD1, 0xF9, 0xC4, 0x60, 0xE4};  // backup auto 
-
+// type your super secret keys here (16 bytes each)
+static const char *PMK_KEY_STR = "laskakit_pmk_key";
+static const char *LMK_KEY_STR = "laskakit_lmk_key";
 esp_now_peer_info_t peerInfo;
 
 struct Message {
   LaskaGamepad gamepad_inputs;
-  uint8_t battery_v;  // voltage * 10
+  uint8_t battery_v; // voltage * 10
   uint8_t off;
 };
 
 Message message;
-
 
 // power off
 int i = 0, ii = 0;
@@ -64,13 +70,13 @@ int last = millis();
 float batt_min = 3.0;
 float batt_warn = 3.2;
 
-
 LaskaGamepad laska_gamepad;
 
-void print_inputs(LaskaGamepad* lg) {
-  int16_t* lgg = (int16_t*)lg;
+void print_inputs(LaskaGamepad *lg) {
+  int16_t *lgg = (int16_t *)lg;
   for (int j = 0; j < 5; j++) {
-    Serial.print(j); Serial.print(" ");
+    Serial.print(j);
+    Serial.print(" ");
     for (int i = 0; i < 16; i++) {
       Serial.print((*lgg & (1 << i)) >> i);
     }
@@ -79,47 +85,7 @@ void print_inputs(LaskaGamepad* lg) {
   }
 }
 
-void print_message(Message* m) {
-  print_inputs(&m->gamepad_inputs);
-}
-
-void setup() {
-  pinMode(OFF_PIN, INPUT);
-
-  Serial.begin(115200);
-  
-  pixels.begin();
-  pixels.setBrightness(BRIGHTNESS);
-
-  adc.attach(ADCpin);  // battery
-
-  // i2c
-  Wire.begin(8, 10);
-  ADS.begin();
-  // voltage on gamepad stick is 0-3V3 (3.31) safe margin
-  // so the raw value is between 0 and 26400 (26480) in theory
-  // there are some deadzones
-  ADS.setGain(1);  // +- 4.096 V
-  PCF.begin();
-
-  pixels.setPixelColor(active_car, pixels.Color(0, 255, 0)); // R, G, B
-  pixels.show();
-
-  WiFi.mode(WIFI_STA);
-  esp_now_init();
-  // esp_now_register_send_cb(OnDataSent);
-
-  memcpy(peerInfo.peer_addr, cars[active_car], 6);
-  // memcpy(peerInfo.peer_addr, cars[0], 6);
-  
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-         
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
-}
+void print_message(Message *m) { print_inputs(&m->gamepad_inputs); }
 
 int16_t handle_deadzones(int16_t value) {
   // deadzone ends 250
@@ -156,104 +122,60 @@ void read_gamepad() {
   laska_gamepad.buttons = ~(PCF.read16());
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  pinMode(OFF_PIN, INPUT);
+
+  Serial.begin(115200);
+
+  pixels.begin();
+  pixels.setBrightness(BRIGHTNESS);
+
+  adc.attach(ADCpin); // battery
+
+  // i2c
+  Wire.begin(8, 10);
+  ADS.begin();
+
+  // voltage on gamepad stick is 0-3V3 (3.31) safe margin
+  // so the raw value is between 0 and 26400 (26480) in theory
+  // there are some deadzones
+  ADS.setGain(1); // +- 4.096 V
+  PCF.begin();
+
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  esp_now_set_pmk((uint8_t *)PMK_KEY_STR);
+
+  // setup peer info
+  memcpy(peerInfo.peer_addr, cars[active_car], 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = true;
+  memcpy(peerInfo.lmk, LMK_KEY_STR, 16);
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
 void loop() {
-  // if (!digitalRead(OFF_PIN)){
-  //   ii++;
-  // }else{
-  //   ii = 0;
-  // }
-
-  // if (ii > 20){
-  //   poweroff = true;
-  //   ii = 0;
-  // }else{
-  //   poweroff = false;
-  // }
-
-  // Serial.print(!digitalRead(OFF_PIN));
-  // Serial.print("  ");
-  // Serial.print(ii);
-  // Serial.print("  ");
-  // Serial.println(poweroff);
-  
-  // ADS.setGain(0);
-
-
   read_gamepad();
-
-  // check for peer select
-  if (laska_gamepad.get_button(LaskaGamepadButton::LEFT)) {
-    esp_now_del_peer(cars[active_car]);
-    active_car = 0;
-  } else if (laska_gamepad.get_button(LaskaGamepadButton::UP)) {
-    esp_now_del_peer(cars[active_car]);
-    active_car = 1;
-  } else if (laska_gamepad.get_button(LaskaGamepadButton::RIGHT)) {
-    esp_now_del_peer(cars[active_car]);
-    active_car = 2;
-  }
-
-  if (laska_gamepad.buttons &
-      (LaskaGamepadButton::LEFT |
-       LaskaGamepadButton::UP |
-       LaskaGamepadButton::RIGHT)) {
-    pixels.clear();
-    pixels.setPixelColor(active_car, pixels.Color(0, 255, 0));
-    memcpy(peerInfo.peer_addr, cars[active_car], 6);
-    esp_now_add_peer(&peerInfo);
-  }
-
-  pixels.show();
-
   message.gamepad_inputs = laska_gamepad;
 
   // read battery
   float batt_voltage = adc.readVoltage() * DividerRatio * 10;
-  uint8_t bv = batt_voltage;
-  // Serial.print(bv); Serial.print(" ");
-  // Serial.println(batt_voltage);
-  message.battery_v = bv;
-  // Serial.print("Battery Voltage = " );
-  // Serial.print(batt_voltage);
-  // Serial.println("V");
-  // Serial.println(sizeof(m));
+  message.battery_v = batt_voltage;
 
-  // if (batt_voltage <= batt_min){
-  //   poweroff = true;
-  // }
-
-  
-
-  // print_message(&message);
-  // Serial.println();
-  esp_err_t result = esp_now_send(cars[active_car], (uint8_t *) &message, sizeof(message));
-  // Serial.print("ESP status: ");
-  // Serial.println(result);
-  delay(10);
+  esp_err_t result = esp_now_send(cars[active_car], (uint8_t *)&message, sizeof(message));
   if (result == ESP_OK) {
+    pixels.setPixelColor(1, pixels.Color(0, 255, 0)); // R, G, B
+    pixels.show();
     Serial.println("Sent with success");
-  }else{
+  } else {
+    pixels.setPixelColor(1, pixels.Color(255, 0, 0)); // R, G, B
+    pixels.show();
     Serial.println("Error sending the data");
   }
 
-
-  // send success
-  // if (ss){
-  //   pixels.setPixelColor(1, pixels.Color(0, 255, 0)); // R, G, B
-  //   pixels.show();
-  // }else{
-  //   pixels.setPixelColor(1, pixels.Color(255, 0, 0)); // R, G, B
-  //   pixels.show();
-  // }
-
-
-  // // turn off if the battery voltage is low
-  // if (batt_voltage <= batt_min){
-  //   pinMode(OFF_PIN, OUTPUT);
-  //   digitalWrite(OFF_PIN, LOW);
-  // } else if (batt_voltage <= batt_warn) {
-  //   pixels.setPixelColor(2, pixels.Color(255, 0, 0));
-  //   pixels.show();
-  // }
+  delay(10);
 }
